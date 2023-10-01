ada24ce48b3607842ea00c10e3b621711830f720
Nan Ma <nanma@microsoft.com>
2023-10-01 15:19:33 -0700

    update python script for 12color

--- 12color/prog_12color.py
+++ 12color/prog_12color.py
@@ -11,16 +11,19 @@ filename_current = 'solving.log'
 if len(sys.argv) == 2:
   filename, filename_current = sys.argv
 
-n_stickers_per_cell = 1 + 7 + 21 + 14 # total = 43
+n_stickers_per_cell = 1 + 9 + 27 + 18 # total = 55
 n_cells = 12
 n_characters_per_sticker = 1
 line_number_timer = 5
 
+# print the clean and dirty indices
+debug_mode = False
+
 # the following is wrong
-index_cell = [42] # 1
-index_face = [0, 6, 12, 18, 24, 30, 36] # 7
-index_edge = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41] # 21
-index_vertex = [2, 4, 8, 10, 14, 16, 20, 22, 26, 28, 32, 34, 38, 40] # 14
+index_cell = [54] # 1
+index_face = [0, 36, 6, 42, 12, 48, 18, 24, 30] # 9
+index_edge = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53] # 27
+index_vertex = [2, 4, 8, 10, 14, 16, 20, 22, 26, 28, 32, 34, 38, 40, 44, 46, 50, 52] # 18
 
 index_types = [
   {'name': 'Cells', 'ind': index_cell, 'nColor':1},
@@ -81,6 +84,15 @@ def compare_status():
           break
       if flag:
         completedType [type['name']] += 1
+
+  if debug_mode:
+    print('Dirty indices:')
+    print(sorted(list(incomplete_indices)))
+    print(len(incomplete_indices))
+    print('Clean indices:')
+    clean_indices = set(range(n_stickers_per_cell)) - incomplete_indices
+    print(sorted(list( clean_indices )))
+    print(len(clean_indices))
 
   print('----------------------------------------------------------------------------')
   print('Type \tSolved Pieces\tSolved Stickers\tPercentage\tCompleted Cells')
